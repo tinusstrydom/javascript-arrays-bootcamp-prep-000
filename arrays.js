63a3c6a8f4e4d8f44dae517899f5757a691d9d34
Tinus Strydom <tinuss@loans-inc.co.za>
2019-06-09 10:41:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
   array.pop();
   return array;
 }
+function removeElementFromEndOfArray(array){
+  array.slice(0, array.lenght-1);
+  return array;
+}
